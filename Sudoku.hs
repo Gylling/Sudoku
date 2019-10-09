264702417bf886af3f980f664becad11be7c090d
DeinaStudent <s174258@student.dtu.dk>
2019-10-09 10:38:09 -0700

    Interface implemented, no more errors - still need to test

--- Sudoku.hs
+++ Sudoku.hs
@@ -4,7 +4,11 @@
 module Sudoku where
 
 import SudokuBoard
+import System.IO
 
+{-
+Type and data definitions.
+-}
 data State = State InternalState  -- internal_state
          deriving (Ord, Eq, Show)
 
@@ -25,15 +29,19 @@ data Action = Action (Int, Int) Int            -- a move for a player is a pair 
 
 type InternalState = ([[Int]],[[Int]],Int, Int)          -- ([board], [solved board], mistakes, difficulty)
 
+
 sudoku :: Game
-sudoku (Action (row,col) move) (State  (board, winBoard,mistakes,diff))
-    | board == winBoard = EndOfGame 1 (sudoku_start (mod diff 3))
-    | mistakes >= 3 = EndOfGame (-1) (sudoku_start diff)
-    | checkInput (row,col) move = ContinueGame(State (board, winBoard, mistakes, diff))
-    | validatePos (row,col) board = ContinueGame(State (board, winBoard, mistakes, diff))
-    | checkMove (row,col) move winBoard = ContinueGame (State ((insertMove board move (row,col)),winBoard, mistakes, diff))
+sudoku (Action (row,col) move) (State  (board, winBoard, mistakes, diff))
+    | (board == winBoard) = EndOfGame 1 (State (board, winBoard, mistakes, diff))
+    | (mistakes >= 3) = EndOfGame (-1) ((State (board, winBoard, mistakes, diff)))
+    | (checkInput (row, col) move) = (ContinueGame (State (board, winBoard, mistakes, diff)))
+    | (validatePos (row,col) board) = ContinueGame (State (board, winBoard, mistakes, diff))
+    | (checkMove (row,col) move winBoard) = ContinueGame (State ((insertMove board move (row,col)), winBoard, mistakes, diff))
     | otherwise = ContinueGame (State (board, winBoard, mistakes+1, diff))
 
+{-
+Validation methods for the game logic.
+-}    
 checkInput :: (Ord a1, Ord a2, Ord a3, Num a1, Num a2, Num a3) => (a1, a2) -> a3 -> Bool
 checkInput (row,col) move = row >8 || row <0 || col >8 || col <0 || move >9 || move <1
 
@@ -49,10 +57,57 @@ insertMove board move (row,col) =
   [take col (board !! row) ++ [move] ++ drop (col + 1) (board !! row)] ++
   drop (row + 1) board
 
--- start state
-sudoku_start :: Int -> State
-sudoku_start diff = State (createBoard diff)
 
+{-
+Interface logic for main menu.
+-}
+game_start :: Double -> IO Integer
+game_start winOrLose =
+    do
+        if winOrLose == 1
+            then putStrLn("Congrats! You won the game. Let's play again.")
+        else if winOrLose == (-1)
+            then putStrLn("Sorry, too many mistakes. You lost. Try again.")
+        else putStrLn("Welcome to Sudoku.")
+        putStrLn("What level do you wish to play? 0. Easy, 1. Medium, 2. Difficult.")
+        level <- getLine --prompt for menu choice
+        if level == "3"
+            then return (-1)
+        else game_play (ContinueGame (State (createBoard level)))
+           
+
+{-
+Interface logic for game.
+Game :: The original information about the game.
+State :: Current game state.
+-}
+game_play :: Result -> IO Integer
+game_play (EndOfGame val state) = (game_start val) -- Game ended (either lost or won)
+game_play (ContinueGame state) = --Game ongoing
+   do
+      let State (board, winBoard, mistakes, difficulty) = state --getting individual components of state
+ --     putStrLn ("This is your current sudoku board: ")
+   --   (printBoard board)
+      putStrLn("Choose row:")
+      x <- getLine
+      putStrLn("Choose column:")
+      y <- getLine
+      putStrLn("Choose number:")
+      no <- getLine
+      game_play (sudoku (Action ((strToInt x), (strToInt y)) (strToInt no)) (state)) 
+
+      
+strToInt s = read s :: Int
+
+--printBoard arr =
+  --  unlines [unwords [show (arr ! (x, y)) | x <- [0..9]] | y <- [0..9]]
+
+
+-- start state
+--sudoku_start :: Int -> State
+--sudoku_start diff = State (createBoard diff)
 
 instance Show Action where
     show (Action (row,col) x) = show x ++ " inserted at " ++ show (row,col)
+
+    --            then computer_play game (ContinueGame start_state) opponent (State (createBoard diff))
